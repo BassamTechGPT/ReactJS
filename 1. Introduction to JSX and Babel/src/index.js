9d081e573cca81e496e9eb7f0482c14981b5bb58
BassamTechGPT <130410203+BassamTechGPT@users.noreply.github.com>
2023-08-24 09:57:09 +0500

    Add files via upload

---
+++ 1. Introduction to JSX and Babel/src/index.js
@@ -0,0 +1,15 @@
+//New Module
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Old Module
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
